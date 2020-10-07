283f7d09582bf2fa6fa2f46a42af94356eec03a9
Daniel <dacevedosp@gmail.com>
2020-10-07 17:46:04 -0300

    Basico Variables_tiposDeDatos

---
+++ Variables/variables_tipos_de_datos.py
@@ -0,0 +1,25 @@
+"""
+Variables y Tipos de Datos
+
+"""
+
+texto = "Soy un texto"
+numero = 10
+decimal = 10.5
+verdadero = True
+numero2 = f"{numero} y {decimal}"
+numero3 = numero + 1
+
+# Formas de imprimir
+print(numero)
+print(f"{numero}")
+print(":",numero)
+
+#Concatenar 
+print(f"Los numeros son: {numero} y {decimal}")
+print("El numero decimal es:",decimal)
+
+#Tipos de datos
+print(type(numero))
+print(type(decimal))
+print(type(verdadero))
