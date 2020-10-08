54461f01d997e6c40b2e4acb64d056df88593590
Daniel <dacevedosp@gmail.com>
2020-10-08 20:56:55 -0300

    Entrada de datos

---
+++ Entrada_de_datos/entra_de_datos.py
@@ -0,0 +1,10 @@
+"""
+Para la entrada de datos
+"""
+
+numero_1 = int(input())
+numero_1 = int(input("Ingresa numero2: "))
+palabra = input()
+palabra2 = input("Ingresa palabara: ")
+decimal = float(input())
+decimal2 = float(input("Ingresa decimal: "))
