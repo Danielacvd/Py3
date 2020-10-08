8e86ae653a744729dda670a4f3e3d556a7dbac3c
Daniel <dacevedosp@gmail.com>
2020-10-08 20:39:08 -0300

    Operadores

---
+++ Operadores/operadores.py
@@ -0,0 +1,32 @@
+"""
+Aritmeticos
+
+Suma: +
+Resta: -
+Multiplicacion: *
+Division: /
+Division Entera: //
+Modulo de la division: %
+Potencia: **
+
+"""
+
+"""
+Logicos
+
+y: and
+o: or
+negacion: not
+
+"""
+
+"""
+Incremente/Decremento
+
+y += 1
+y *= 1
+y /= 1
+y **= 1
+y -=1
+
+"""
