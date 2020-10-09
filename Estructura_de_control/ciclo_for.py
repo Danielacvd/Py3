f88d3e3f67e95910f37757bfa08b01e61881f38f
Daniel <dacevedosp@gmail.com>
2020-10-08 21:15:59 -0300

    Estructura de control For

---
+++ Estructura_de_control/ciclo_for.py
@@ -0,0 +1,17 @@
+"""
+Ciclo For
+
+for variable in elemento_iterable:
+    instrucciones
+else:
+    print("Fin ciclo")    
+"""
+
+
+numeros = [1,2,3,4,5,6]
+for i in numeros:
+    print(i)
+
+palabra = "Dinosaurio"
+for i in palabra:
+    print(i)    
