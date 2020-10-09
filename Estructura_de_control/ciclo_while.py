159ee3eb5f52937ed7133e5925879f8357b9a5c5
Daniel <dacevedosp@gmail.com>
2020-10-08 21:20:57 -0300

    Ciclo While

---
+++ Estructura_de_control/ciclo_while.py
@@ -0,0 +1,17 @@
+"""
+Ciclo While
+
+inicializo variable
+while condicion:
+    instrucciones
+    actualizar variable
+else:
+    print("Fin ciclo")
+
+"""
+
+i = 0
+while i <= 10:
+    print(i)
+    i += 1
+    
