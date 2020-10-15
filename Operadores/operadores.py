3fa192103cf4595b23d24224857ed40b451e0c6f
Daniel <dacevedosp@gmail.com>
2020-10-14 22:07:38 -0300

    Update operadores

--- Operadores/operadores.py
+++ Operadores/operadores.py
@@ -15,8 +15,8 @@ Potencia: **
 Logicos
 
 y: and
-o: or
-negacion: not
+o: or 
+negacion: not (Niego una expresion logica o un valor, invierto una verdad, doy vuelta un resultado, una comparacion)
 
 """
 
@@ -41,4 +41,13 @@ menor igual: <=
 mayor: >
 mayor igual: >=
 
+"""
+
+"""
+True equivale a 1
+False equivale a 0
+"""
+
+"""
+Siempre puedo ir anidando todas estas expresiones
 """
