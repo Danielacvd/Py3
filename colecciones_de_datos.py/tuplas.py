1b39716222a9a8354e27ae192617a5d61f136634
Daniel <dacevedosp@gmail.com>
2020-10-15 17:07:12 -0300

    Colecciones de datos TUPLAS

---
+++ colecciones_de_datos.py/tuplas.py
@@ -0,0 +1,13 @@
+"""
+Tuplas en Python3
+Son variables contenedoras parecidas a las listas, pero las tuplas sin inmutables
+"""
+
+tupla = (100,"Hola",[1,2,3],-50)
+
+print(tupla)
+print(tupla[0])
+print(tupla[-1])
+print(tupla[2:])
+print(tupla[2][-1])
+
