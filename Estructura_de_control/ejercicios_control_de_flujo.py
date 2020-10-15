c52c9e051e874cf536237c5885725504c210c9e2
Daniel <dacevedosp@gmail.com>
2020-10-15 17:01:03 -0300

    Update ejercicios Estructuras de control ejericico 7 done

--- Estructura_de_control/ejercicios_control_de_flujo.py
+++ Estructura_de_control/ejercicios_control_de_flujo.py
@@ -120,3 +120,20 @@ print(lista_2)
 print(lista_3)
 print(lista_4)
 print(lista_5)
+
+print("*** Ejercicio 7 ***")
+
+"""
+Ejercicio 7
+Dadas dos listas, debes generar una tercera con todos los elementos que se repitan en ellas, pero no debe repetirse ningún elemento en la nueva lista:
+"""
+
+lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
+lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
+
+lista_3 = []
+for letra in lista_1:
+    if letra in lista_2 and letra not in lista_3:
+        lista_3.append(letra)
+
+print(lista_3)
