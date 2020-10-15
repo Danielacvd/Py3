1cd5a46e306a7a466b84ff02a48b27b1a56c9c22
Daniel <dacevedosp@gmail.com>
2020-10-14 22:31:15 -0300

    Update Estructura de control WHile

--- Estructura_de_control/ciclo_while.py
+++ Estructura_de_control/ciclo_while.py
@@ -8,6 +8,12 @@ while condicion:
 else:
     print("Fin ciclo")
 
+Si uso el break en el while, no se ejecutara el else, ya que el ciclo no finaliza su iteracion, si no que se interrumpe
+
+Instrucción continue
+Sirve para "saltarse" la iteración actual sin romper el bucle.
+
+Hacer menus, dentro de un while van las opciones, como el do while de C
 """
 
 i = 0
