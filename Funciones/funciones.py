82787d9bc44f28d83525caf23560356ead281940
Daniel <dacevedosp@gmail.com>
2020-10-21 21:48:14 -0300

    Update Funciones Envio de Valores

--- Funciones/funciones.py
+++ Funciones/funciones.py
@@ -11,10 +11,29 @@ def nombre_funcion():
 
 nombre_funcion()    
 
-
 Dentro de la funcion puedo declarar variables, inicializarlas, etc
 Pero para que estan variables puedan vivir fuera de la funcion, las tengo que retornar
 Las variables solo viven dentro de la funcion, si declaro una variable dentro de una funcion y la llamo desde fuera de la funcion dara un error, por eso se escapan o retornan usando el return o print.
+
+Retorno:
+Lo importante del return es que con el puedo sacar devolver valores, al momento de devolver valores la ejecucion de la funcion termina.
+Puedo devolver multiples valores, separandolos por comas. Pero estos valores se trataran en conjunto como su fueran una tupla, inmutables, pero se pueden reasignar a otras variables.
+
+Envio de valores
+Ademas de devolver valores, puedo recibir valores
+Syntax:
+def nombre_funcion(a, b):
+    resultado = a+b
+    return resultado
+
+nombre_funcion(a, b) #No necesariamente el nombre de la variable que entrego tiene que ser el que recibo en los parametros de la funcion.
+
+def suma(a, b):
+    return a+b
+
+resultado = suma(5,5)
+print(resultado)
+
 
 
 """
@@ -36,3 +55,11 @@ def parametro_por_defecto(nombre = "Daniel"):
     return f"Hola {nombre}"
 
 print(parametro_por_defecto("Feña"))
+
+
+def suma(a, b):
+    return a+b
+
+resultado = suma(5,5)
+print(resultado)
+
