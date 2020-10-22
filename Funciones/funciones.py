116b563334b54c0f06587b7ff4c010a569ba5c2f
Daniel <dacevedosp@gmail.com>
2020-10-21 21:37:37 -0300

    Update Funciones

--- Funciones/funciones.py
+++ Funciones/funciones.py
@@ -1,6 +1,21 @@
 """
 Funciones en python3
 
+Syntax Basica:
+def nombre_funcion():
+    codigo....
+    return codigo
+
+#llamo desde el cuerpo del programa
+
+nombre_funcion()    
+
+
+Dentro de la funcion puedo declarar variables, inicializarlas, etc
+Pero para que estan variables puedan vivir fuera de la funcion, las tengo que retornar
+Las variables solo viven dentro de la funcion, si declaro una variable dentro de una funcion y la llamo desde fuera de la funcion dara un error, por eso se escapan o retornan usando el return o print.
+
+
 """
 
 def nombre_funcion(parametro):
