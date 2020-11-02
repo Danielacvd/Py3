f7ee42d7839a01a1d00cc3e2f4e37bb856c6a101
Daniel <dacevedosp@gmail.com>
2020-11-01 21:54:04 -0300

    Errores y Excepciones Invocacion de Excepciones

--- Errores_y_excepciones/invocacion_de_excepciones.py
+++ Errores_y_excepciones/invocacion_de_excepciones.py
@@ -1,6 +1,28 @@
 """
 Errores y Excepciones Python3
 Invocacion de excepciones
+Por si necesitamos llamar a un error manualmente y no usar un simple print()
+
+Instruccion Raise
+Con raise se puede lanzer un error manual pasandole el identificador. Luego annadimos un except para tratar esta excepcion que se hemos lanzado.
 
 
 """
+
+def mi_funcion(algo=None):
+    if algo is None:
+        print("Error no se permite un valor nulo (con un print)")
+
+mi_funcion()
+
+#Con Raise
+
+def mi_funcion(algo=None):
+    try:
+        if algo is None:
+            raise ValueError("Error! No se permite un valor nulo")
+    except ValueError:
+        print("Error! No se permite un valor nulo (desde la excepción)")
+
+mi_funcion()
+
