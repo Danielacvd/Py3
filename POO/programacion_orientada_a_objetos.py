8bf51e154b17287fca7eba9b2825957f6616e078
Daniel <dacevedosp@gmail.com>
2020-11-08 22:29:15 -0300

    Poo python

---
+++ POO/programacion_orientada_a_objetos.py
@@ -0,0 +1,9 @@
+"""
+Poo en Python3
+Se basa en los objetos, estos objetos son de un tipo de dato que vienen desde una Clase.
+Las Clases son nuestros moldes(de autos, pasteles...), los objetos son los autos y los pasteles ya creados(instanciados).
+Todos los objetos salen de un solo molde, tienen la misma base, pero a nivel de atributos seran muy diferentes, cantidad de puestas, color, tamano, peso...
+
+
+
+"""
